266e11e0162244b92c1fa68134f38ed09667b0f6
pchan <221870089@smail.nju.edu.cn>
2023-01-11 22:56:00 +0800

    test : delete TODO

--- ramfs.c
+++ ramfs.c
@@ -2,35 +2,27 @@
 /* modify this file freely */
 
 int ropen(const char *pathname, int flags) {
-  // TODO();
 }
 
 int rclose(int fd) {
-  // TODO();
 }
 
 ssize_t rwrite(int fd, const void *buf, size_t count) {
-  // TODO();
 }
 
 ssize_t rread(int fd, void *buf, size_t count) {
-  // TODO();
 }
 
 off_t rseek(int fd, off_t offset, int whence) {
-  // TODO();
 }
 
 int rmkdir(const char *pathname) {
-  // TODO();
 }
 
 int rrmdir(const char *pathname) {
-  // TODO();
 }
 
 int runlink(const char *pathname) {
-  // TODO();
 }
 
 void init_ramfs() {
