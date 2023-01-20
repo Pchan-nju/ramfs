a6c76f32309a83c43fb9e2df8f72e62e650a8f73
pchan <221870089@smail.nju.edu.cn>
2023-01-20 17:19:36 +0800

    compile

--- ramfs.c
+++ ramfs.c
@@ -197,7 +197,7 @@ int ropen(const char *pathname, int flags) {
             return des_cnt;
         }
         else {
-            printf("Error : there is no such file and it is not allowed to create.\n");
+            printf("Error : there is no \"%s\" and it is not allowed to create.\n", str);
             return -1;
         }
     }
